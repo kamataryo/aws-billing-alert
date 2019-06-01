34f7a04c88e98f0e48e336a96913105721875713
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2019-06-01 21:44:34 +0900

    Fix lambda logging error

--- index.js
+++ index.js
@@ -74,10 +74,18 @@ module.exports.handler = async () => {
     }
   }
 
-  return axios.post(process.env.SLACK_INCOMMING_WEBHOOK_URL, {
-    channel: '#general',
-    username: 'AWS請求bot',
-    text,
-    icon_emoji: ':ghost:'
-  })
+  const slackArg = [
+    process.env.SLACK_INCOMMING_WEBHOOK_URL,
+    {
+      channel: '#general',
+      username: 'AWS請求bot',
+      text,
+      icon_emoji: ':ghost:'
+    }
+  ]
+  try {
+    await axios.post(...slackArg)
+  } catch (error) {
+    console.error({ error, slackArg })
+  }
 }
